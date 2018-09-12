cbf99e35803838a3a000a065dc2d7212aa8cb155
Sam Sheffres <ssheffres@gmail.com>
2018-09-13 05:05:25 +0700

    Use new URL + add support for new fields + use default values for null

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,55 @@ use threadpool::ThreadPool;
 
 extern crate reqwest;
 
-extern crate clap;
-use clap::{Arg, App};
-
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
+use serde::{Deserialize, Deserializer};
+
+
+fn default_resource() -> String {
+    "".to_string()
+}
+
+fn nullable_resource<'de, D>(deserializer: D) -> Result<String, D::Error>
+    where D: Deserializer<'de>
+{
+    let opt = Option::deserialize(deserializer)?;
+    Ok(opt.unwrap_or_else(default_resource))
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 struct Package {
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     track_id: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     NameIM: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     ProgramNumber: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     order_id: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     Weight: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     delivery_type: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     ForingParcel: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     point_city: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     point_address: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     point_phone: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     Code: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     store_date: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
     delivery_date: String,
+    #[serde(default = "default_resource", deserialize_with = "nullable_resource")]
+    ref_track: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -35,7 +61,7 @@ struct TrackingCode {
     data: Option<Vec<Package>>,
 }
 
-pub static BASEURL: &str = "http://boxberry.ru/tracking.service/tracking.php?check_captcha=1&g_recaptcha_response=0";
+pub static BASEURL: &str = "https://boxberry.ru/tracking.service/tracking_service.php?check_captcha=1&g_recaptcha_response=0";
 pub static LETTERS: &'static [&str] = &["A", "B", "C", "D"];
 
 fn main() {
@@ -69,7 +95,7 @@ fn print_packages(tracking_code: TrackingCode) {
         let result = tracking_code.data.unwrap();
 
         for item in result {
-            println!("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}", item.track_id, item.NameIM, item.ProgramNumber, item.order_id, item.Weight, item.delivery_type, item.ForingParcel, item.point_city, item.point_address, item.point_phone, item.Code, item.store_date, item.delivery_date);
+            println!("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}", item.track_id, item.NameIM, item.ProgramNumber, item.order_id, item.Weight, item.delivery_type, item.ForingParcel, item.point_city, item.point_address, item.point_phone, item.Code, item.store_date, item.delivery_date, item.ref_track);
         }
     }
 }
